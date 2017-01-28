ca8c414065e75fc0c55061dcc24fd086dec6f763
jiro-sains <jrosains@gmail.com>
2017-01-28 16:18:45 +0000

    fonts ststill not working

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,10 @@
-require_relative 'boot'
+require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module Splurty
   class Application < Rails::Application
