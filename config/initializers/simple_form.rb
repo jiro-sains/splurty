08f9ca35e7a155831e0de315bae396c1e5bffe1b
jiro-sains <jrosains@gmail.com>
2017-01-28 22:40:54 +0000

    validation added

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -166,4 +166,6 @@ SimpleForm.setup do |config|
 
   # Defines which i18n scope will be used in Simple Form.
   # config.i18n_scope = 'simple_form'
+
+  config.label_text = lambda { |label, required, explicit_label| "#{} #{label}" }
 end
